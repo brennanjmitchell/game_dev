6dfca5b674e9ff55ffefc46190b157f18b3630b5
Brennan Mitchell <brennanjmitchell@gmail.com>
2024-06-02 15:49:27 -0700

    Initial npm setup with custom scripts, http-server and nodemon packages, and initial package.json file

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const scene = new THREE.Scene();
 
 // Create a geometry and a material, then combine them into a mesh
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const cube = new THREE.Mesh(geometry, material);
 
 // Add the cube to the scene
@@ -23,6 +23,15 @@ loader.load(
         console.error('An error occurred while loading the model:', error);
     }
 );
+
+// Test initial Point light
+const pointLight = new THREE.PointLight(0xffffff, 1, 100); // Color, Intensity, Distance
+pointLight.position.set(10, 10, 10);
+scene.add(pointLight);
+
+// Ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Color, Intensity
+scene.add(ambientLight);
 
 // Initialize the renderer and add it to the DOM
 const renderer = initRenderer();
